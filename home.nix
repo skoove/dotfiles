b47ec9571b322af92e49731e14155fcf07d54df6
zie <fuuck you>
2024-06-26 14:50:17 +1000

    tweaked home manager

--- home.nix
+++ home.nix
@@ -34,7 +34,6 @@
     xorg.xev                # for checking keycodes mostly
     gamemode                # makes games run better (?)
     dconf
-    lxappearance
     catppuccin-cursors.mochaLavender
     unzip
     tree
@@ -47,7 +46,7 @@
 
   catppuccin = {
     enable = true;
-    accent = "rosewater";
+    accent = "lavender";
     flavor = "mocha";
   };
 
@@ -65,16 +64,6 @@
         shell.program = "${pkgs.zsh}/bin/zsh";
         window.padding = {x = 10; y = 10;};
         font.normal = {family = "JetBrainsMonoNL Nerd Font";};
-        # hey future zie,, i know your reading this because
-        # you were trying to workout why your alacritty is
-        # not sexy, and its because the theme needs to be downloaded
-        # just run
-        # curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
-        # to get your beautiful mocha back
-        # not working?
-        # check here!
-        # https://github.com/catppuccin/alacritty
-        import = [ "~/.config/alacritty/catppuccin-mocha.toml" ];      
       };
     };   
     
@@ -151,42 +140,10 @@
     };
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home.file = {};
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+  home.sessionVariables = {};
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/zie/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
 }
