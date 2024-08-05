7204df160bcd324153858c80df6350de294bb699
zie <zac@sturges.com.au>
2024-08-06 06:46:52 +1000

    flake updated

--- home.nix
+++ home.nix
@@ -118,6 +118,7 @@
           cd ~/.dotfiles
           nix flake update ~/.dotfiles
           echo "flake updated"
+          git commit -a -m "flake updated"
         '';
 
         upd-home = ''
