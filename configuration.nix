d5fd81b6177d5b204b98ead7a1311233c780c167
skoove <zie@skoove.dev>
2025-03-09 19:12:28 +1000

    init 2 electric boogaloo

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -12,16 +8,26 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # storage optimisation
+  nix.optimise = {
+    automatic = true;
+    persistent = true;
+    dates = [ "weekly" ];
+  };
+
+  nix.gc = {
+    automatic = true;
+    persistent = true;
+    dates = "daily";
+    options = "--delete-older-than 5d";
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -44,12 +50,6 @@
     LC_TIME = "en_AU.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -72,7 +72,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-  #
 
   users.defaultUserShell = pkgs.nushell;
 
@@ -82,12 +81,12 @@
     description = "Zie Sturges";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  environment.sessionVariables = {
+    EDITOR = "helix";
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -95,26 +94,22 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    firefox
     nushell
     helix
     nh
     git
     nil
-    starship
+    hyprland
+    kitty
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.hyprland.enable = true;
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  services.displayManager.ly.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22000 ];
