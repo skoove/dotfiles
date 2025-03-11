9ac070b3586f11527d2a5f5a08faad8d8b2a4282
Zie Sturges <53106860+skoove@users.noreply.github.com>
2025-03-11 14:42:31 +1000

    added fire animation to ly

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,8 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
   services.displayManager.ly.enable = true;
+  services.displayManager.ly.settings.animation = "doom";
+  
   services.xserver.desktopManager.gnome.enable = true;
 
   # Open ports in the firewall.
