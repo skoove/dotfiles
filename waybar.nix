6454fbbd6f2758cf804b39bc2c95dc4de4ee49d0
skoove <zie@skoove.dev>
2025-03-16 20:34:01 +1000

    changed background color of waybar

---
+++ waybar.nix
@@ -0,0 +1,20 @@
+{ config, ... }:
+let
+  colors = config.lib.stylix.colors;
+in
+{
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true; # makes it start with hyprland
+
+    settings.main-bar = {
+      modules-left = [ "hyprland/workspaces" "hyprland/window" ];
+    };
+
+    style = ''
+      window#waybar {
+        background-color: #${colors.base01};
+      }
+    '';
+  };
+}
