6134b2b5e72f077c3185138da3517e5552272460
Zie Sturges <53106860+skoove@users.noreply.github.com>
2025-03-18 07:32:20 +1000

    fixup! fixup! per device input config hyprland

--- hyprland.nix
+++ hyprland.nix
@@ -13,9 +13,9 @@ in {
     misc.disable_hyprland_logo = true;
   
     monitor = [
-    ", preferred, auto, 1" # default for unspecified monitors
-    "HDMI-A-1, 1920x1080@100.0, auto, 1" # main desktop monitor
-    "DVI-D-1, 1920x1080@100.0, auto-left, 1" # left desktop monitor
+      ", preferred, auto, 1" # default for unspecified monitors
+      "HDMI-A-1, 1920x1080@100.0, auto, 1" # main desktop monitor
+      "DVI-D-1, 1920x1080@100.0, auto-left, 1" # left desktop monitor
     ];
 
     device = {
