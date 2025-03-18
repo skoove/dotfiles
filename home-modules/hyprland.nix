d22e23e265d97a71415d97ccc6f753c29326422b
skoove <zie@skoove.dev>
2025-03-18 20:41:21 +1000

    multimedia keys

--- home-modules/hyprland.nix
+++ home-modules/hyprland.nix
@@ -37,6 +37,15 @@ in {
       "${mod}, mouse:273, resizewindow"
     ];
 
+    bindel = [
+      ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+      ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+      ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+      ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+    ];
+
     bind = [
       "${mod}, T, exec, bemoji -c -n"
       "${mod}, D, exec, ${menu}"
