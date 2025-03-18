5414fc5b1cdc1ab6d080058a8bc3101b6f37b27c
Zie Sturges <53106860+skoove@users.noreply.github.com>
2025-03-18 11:20:00 +1000

    reorginised home.nix

--- home.nix
+++ home.nix
@@ -8,15 +8,6 @@
 
   imports = [ ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   fonts.fontconfig.enable = true;
   
   home.packages = with pkgs; [
@@ -53,4 +44,5 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
+  home.stateVersion = "24.11";
 }
