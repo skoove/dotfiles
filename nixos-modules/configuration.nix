caa9661f8e39730513d9e508f049a3a85ea5ea07
skoove <zie@skoove.dev>
2025-03-21 15:27:18 +1000

    nixos: change default shell to zsh

--- nixos-modules/configuration.nix
+++ nixos-modules/configuration.nix
@@ -86,7 +86,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  users.defaultUserShell = pkgs.nushell;
+  users.defaultUserShell = pkgs.zsh;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zie = {
