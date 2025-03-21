61805c76a5f0104f5b5843ec767a0b328949402c
skoove <zie@skoove.dev>
2025-03-21 20:06:02 +1000

    hyprland: add steam & discord to exec-once
    I was going to find a way to make this happen on desktop, but not laptop
    but I really cannot be bothered at the moment, and I dont think they are
    going to use too many resources anyway.

--- home-modules/hyprland.nix
+++ home-modules/hyprland.nix
@@ -46,6 +46,8 @@ in {
       "systemctl --user enable --now hyprpaper.service"
       "systemctl --user enable --now waybar.service"
       "systemctl --user enable --now syncthingtray.service"
+      "discord --start-minimized"
+      "steam -silent"
     ];
 
     animation = [
