a06d6460c220731bb8b7ec391e38b1dfd3bf6464
skoove <zie@skoove.dev>
2025-03-24 19:28:19 +1000

    dwarf-fortress: turned off uneccercary programs

--- home-modules/dwarf-fortress.nix
+++ home-modules/dwarf-fortress.nix
@@ -3,12 +3,14 @@
 {
   home.packages = [
     (pkgs.dwarf-fortress-packages.dwarf-fortress-full.override {
-      dfVersion = "0.47.05";
-      theme = pkgs.dwarf-fortress-packages.themes.phoebus;
-      enableIntro = false;
-      enableDFHack = true;
-      enableTWBT = true;
-      enableTruetype = true;
+    dfVersion = "0.47.05";
+    theme = pkgs.dwarf-fortress-packages.themes.afro-graphics;
+    enableIntro = false;
+    enableDFHack = true;
+    enableTWBT = true;
+    enableTruetype = true;
+    enableDwarfTherapist = false;
+    enableLegendsBrowser = false;
     })
   ];
 
