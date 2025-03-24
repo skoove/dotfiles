c2fdbb04bac31e6224e49b3cfca34a596e5febae
skoove <zie@skoove.dev>
2025-03-24 17:41:03 +1000

    dwarf-fortress: changed theme

--- home-modules/dwarf-fortress.nix
+++ home-modules/dwarf-fortress.nix
@@ -4,11 +4,11 @@
   home.packages = [
     (pkgs.dwarf-fortress-packages.dwarf-fortress-full.override {
       dfVersion = "0.47.05";
-      theme = pkgs.dwarf-fortress-packages.themes.wanderlust;
+      theme = pkgs.dwarf-fortress-packages.themes.phoebus;
       enableIntro = false;
       enableDFHack = true;
       enableTWBT = true;
-      enableTruetype = false;
+      enableTruetype = true;
     })
   ];
 
