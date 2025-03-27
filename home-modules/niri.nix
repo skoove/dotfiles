7c9218676ce02abcd8cbc38f2e1e2286b959b5da
skoove <zie@skoove.dev>
2025-03-27 18:02:39 +1000

    niri: add laptop monitor config

--- home-modules/niri.nix
+++ home-modules/niri.nix
@@ -74,6 +74,13 @@ in {
           };
         }
       ];
+
+      outputs = {
+        "Chimei Innolux Corporation" = {
+          scale = 1.0;
+          mode = "1920x1080@60";
+        };
+      };
       
       binds = with config.lib.niri.actions;
       let
