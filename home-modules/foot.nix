32090ed4a48ec0c5d491b1f2e694b8fad89115a0
skoove <zie@skoove.dev>
2025-03-28 09:27:24 +1000

    starship: create custom prompt :D

--- home-modules/foot.nix
+++ home-modules/foot.nix
@@ -4,6 +4,7 @@
     enable = true;
     settings = {
       key-bindings.spawn-terminal = "Mod1+n";
+      main.pad = "3x3 center";
     };
   };
 }
--- home-modules/starship.nix
+++ home-modules/starship.nix
@@ -6,247 +6,41 @@
     enable = true;
     enableFishIntegration = true;
     
-    settings = builtins.fromTOML ''
-      [character]
-      success_symbol = "[>](bold green)"
-      error_symbol = "[x](bold red)"
-      vimcmd_symbol = "[<](bold green)"
+    settings = {
+      format = 
+        "$username$hostname" +
+        "$directory" +
+        "$nix_shell" +
+        "$character";
 
-      [git_commit]
-      tag_symbol = " tag "
+      username = {
+        style_root = "bold red";
+        style_user = "bold yellow";
+        show_always = true;
+        format = "[$user]($style)[@](blue)";
+      };
 
-      [git_status]
-      ahead = ">"
-      behind = "<"
-      diverged = "<>"
-      renamed = "r"
-      deleted = "x"
+      hostname = {
+        ssh_only = false;
+        format = "[$hostname]($style) ";
+        style = "bold green";
+      };
 
-      [aws]
-      symbol = "aws "
+      directory = {
+        fish_style_pwd_dir_length = 5;
+        truncate_to_repo = true;
+        style = "blue";
+        read_only_style = "red";
+        read_only = "";
+      };
 
-      [azure]
-      symbol = "az "
-
-      [buf]
-      symbol = "buf "
-
-      [bun]
-      symbol = "bun "
-
-      [c]
-      symbol = "C "
-
-      [cobol]
-      symbol = "cobol "
-
-      [conda]
-      symbol = "conda "
-
-      [crystal]
-      symbol = "cr "
-
-      [cmake]
-      symbol = "cmake "
-
-      [daml]
-      symbol = "daml "
-
-      [dart]
-      symbol = "dart "
-
-      [deno]
-      symbol = "deno "
-
-      [dotnet]
-      symbol = ".NET "
-
-      [directory]
-      read_only = " ro"
-
-      [docker_context]
-      symbol = "docker "
-
-      [elixir]
-      symbol = "exs "
-
-      [elm]
-      symbol = "elm "
-
-      [fennel]
-      symbol = "fnl "
-
-      [fossil_branch]
-      symbol = "fossil "
-
-      [gcloud]
-      symbol = "gcp "
-
-      [git_branch]
-      symbol = "git "
-
-      [gleam]
-      symbol = "gleam "
-
-      [golang]
-      symbol = "go "
-
-      [gradle]
-      symbol = "gradle "
-
-      [guix_shell]
-      symbol = "guix "
-
-      [hg_branch]
-      symbol = "hg "
-
-      [java]
-      symbol = "java "
-
-      [julia]
-      symbol = "jl "
-
-      [kotlin]
-      symbol = "kt "
-
-      [lua]
-      symbol = "lua "
-
-      [nodejs]
-      symbol = "nodejs "
-
-      [memory_usage]
-      symbol = "memory "
-
-      [meson]
-      symbol = "meson "
-
-      [nats]
-      symbol = "nats "
-
-      [nim]
-      symbol = "nim "
-
-      [nix_shell]
-      symbol = "nix "
-
-      [ocaml]
-      symbol = "ml "
-
-      [opa]
-      symbol = "opa "
-
-      [os.symbols]
-      AIX = "aix "
-      Alpaquita = "alq "
-      AlmaLinux = "alma "
-      Alpine = "alp "
-      Amazon = "amz "
-      Android = "andr "
-      Arch = "rch "
-      Artix = "atx "
-      CachyOS = "cach "
-      CentOS = "cent "
-      Debian = "deb "
-      DragonFly = "dfbsd "
-      Emscripten = "emsc "
-      EndeavourOS = "ndev "
-      Fedora = "fed "
-      FreeBSD = "fbsd "
-      Garuda = "garu "
-      Gentoo = "gent "
-      HardenedBSD = "hbsd "
-      Illumos = "lum "
-      Kali = "kali "
-      Linux = "lnx "
-      Mabox = "mbox "
-      Macos = "mac "
-      Manjaro = "mjo "
-      Mariner = "mrn "
-      MidnightBSD = "mid "
-      Mint = "mint "
-      NetBSD = "nbsd "
-      NixOS = "nix "
-      Nobara = "nbra "
-      OpenBSD = "obsd "
-      OpenCloudOS = "ocos "
-      openEuler = "oeul "
-      openSUSE = "osuse "
-      OracleLinux = "orac "
-      Pop = "pop "
-      Raspbian = "rasp "
-      Redhat = "rhl "
-      RedHatEnterprise = "rhel "
-      RockyLinux = "rky "
-      Redox = "redox "
-      Solus = "sol "
-      SUSE = "suse "
-      Ubuntu = "ubnt "
-      Ultramarine = "ultm "
-      Unknown = "unk "
-      Uos = "uos "
-      Void = "void "
-      Windows = "win "
-
-      [package]
-      symbol = "pkg "
-
-      [perl]
-      symbol = "pl "
-
-      [php]
-      symbol = "php "
-
-      [pijul_channel]
-      symbol = "pijul "
-
-      [pulumi]
-      symbol = "pulumi "
-
-      [purescript]
-      symbol = "purs "
-
-      [python]
-      symbol = "py "
-
-      [quarto]
-      symbol = "quarto "
-
-      [raku]
-      symbol = "raku "
-
-      [ruby]
-      symbol = "rb "
-
-      [rust]
-      symbol = "rs "
-
-      [scala]
-      symbol = "scala "
-
-      [spack]
-      symbol = "spack "
-
-      [solidity]
-      symbol = "solidity "
-
-      [status]
-      symbol = "[x](bold red) "
-
-      [sudo]
-      symbol = "sudo "
-
-      [swift]
-      symbol = "swift "
-
-      [typst]
-      symbol = "typst "
-
-      [terraform]
-      symbol = "terraform "
-
-      [zig]
-      symbol = "zig "
-    '';
+      character = {
+        format = "$symbol ";
+        success_symbol = "[>](green)";
+        error_symbol = "[>](red)";
+        vimcmd_symbol = "[<](green)";
+        vimcmd_visual_symbol = "[<](yellow)";
+      };
+    };
   };
 }
