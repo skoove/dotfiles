2967c1b241ad2a4c0540b0169ebcd1dc6415767f
skoove <zie@skoove.dev>
2025-03-28 10:36:55 +1000

    home: add ripgrep

--- home-modules/home.nix
+++ home-modules/home.nix
@@ -66,6 +66,7 @@
     bitwarden-cli      # password manager
     sops               # secrets management
     mprocs             # run commands ez
+    ripgrep            # grepper
 
     # not tools at all but i dont want to nix run them
     nitch # pretty, mostly useless
