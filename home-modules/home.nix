92a6cd59492cee3fa1ce99d4603f9a99ee17ce69
skoove <zie@skoove.dev>
2025-04-03 17:15:16 +1000

    whatsapp add

--- home-modules/home.nix
+++ home-modules/home.nix
@@ -70,6 +70,7 @@
     sops               # secrets management
     mprocs             # run commands ez
     ripgrep            # grepper
+    whatsapp-for-linux # whats
 
     # not tools at all but i dont want to nix run them
     nitch # pretty, mostly useless
