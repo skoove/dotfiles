2ea568d79c70d8be8d3c488d6f7a911522d3247b
skoove <zie@skoove.dev>
2025-04-24 14:01:17 +1000

    add file picking fish function
    from https://axlefublr.github.io/uri-list/

--- home-modules/fish.nix
+++ home-modules/fish.nix
@@ -39,6 +39,11 @@
         body = "echo \"command not found :(\"";
       };
 
+      # sorce: https://axlefublr.github.io/uri-list/
+      pick = {
+        body = "echo file://(realpath $argv[1]) | wl-copy -t text/uri-list";
+      };
+
       mk-snippet = {
         body = builtins.readFile ../scripts/mk-snippet.fish;
       };
