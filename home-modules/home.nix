30cb65bc0d73f87fa4395be3897f066a0b954dc3
skoove <zie@skoove.dev>
2025-04-28 09:33:42 +1000

    add zathura

--- home-modules/home.nix
+++ home-modules/home.nix
@@ -78,6 +78,7 @@
     qbittorrent        # :D
     typst              # for writing school papers
     tinymist           # typst lsp
+    zathura            # pdf viewer
 
     # not tools at all but i dont want to nix run them
     nitch # pretty, mostly useless
