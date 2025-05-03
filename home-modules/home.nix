a0588938e9b8a864277b81b39f2cae8cd3d16735
skoove <zie@skoove.dev>
2025-05-03 20:05:28 +1000

    home: add anki

--- home-modules/home.nix
+++ home-modules/home.nix
@@ -79,6 +79,7 @@
     zathura            # pdf viewer
     gnome-pomodoro     # pomodoro
     gh                 # gh cli
+    anki-bin           # flashcards
 
     # not tools at all but i dont want to nix run them
     nitch # pretty, mostly useless
