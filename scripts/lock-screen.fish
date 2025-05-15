7b949c0fc1894eb3cabc16c6f13866926a5ea5f0
skoove <zie@skoove.dev>
2025-05-15 21:10:39 +1000

    lock-screen: turn off screen after 5m

--- scripts/lock-screen.fish
+++ scripts/lock-screen.fish
@@ -4,6 +4,11 @@ function lock
     swaylock --clock --indicator $argv
 end
 
+swayidle timeout 300 "niri msg action power-off-monitors" \
+    resume "niri msg action power-off-monitors" &
+
+set sway_pid $last_pid
+
 if test $hour -ge 21
     # if past 2100h lock to dark color
     lock -c 282828
@@ -11,3 +16,5 @@ else
     # just blur a bit otherwise
     lock --screenshot --effect-pixelate 10
 end
+
+kill $sway_pid
