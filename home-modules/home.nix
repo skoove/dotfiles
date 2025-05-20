b693cba6506ea8bb677d8118b49b58176e077315
skoove <zie@skoove.dev>
2025-05-20 14:28:56 +1000

    add speedcrunch

--- home-modules/home.nix
+++ home-modules/home.nix
@@ -57,7 +57,7 @@
     nom               # rss reader
     blanket           # ambient noise
     krita             # draw
-    numbat
+    numbat speedcrunch
 
     # utils
     bemoji                                           # emoji picker for wofi
