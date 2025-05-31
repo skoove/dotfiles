86b02a29cd03025a768fbcb527e4d1c5a9e19ec3
skoove <zie@skoove.dev>
2025-05-31 21:00:27 +1000

    ausdhuasd

--- home-modules/foot.nix
+++ home-modules/foot.nix
@@ -3,7 +3,7 @@
   programs.foot = {
     enable = true;
     settings = {
-      key-bindings.spawn-terminal = "Mod1+n";
+      key-bindings.spawn-terminal = "Mod1+Shift+n";
       main.pad = "3x3 center";
     };
   };
--- stylix.nix
+++ stylix.nix
@@ -20,7 +20,7 @@
       applications = 10;
       desktop = 10;
       popups = 10;
-      terminal = 10;
+      terminal = 9;
     };
   };
 
