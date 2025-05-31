432c593014e92c725543be4a5653cf2133a301e5
skoove <zie@skoove.dev>
2025-05-31 21:12:10 +1000

    asdun

--- home-modules/foot.nix
+++ home-modules/foot.nix
@@ -3,7 +3,7 @@
   programs.foot = {
     enable = true;
     settings = {
-      key-bindings.spawn-terminal = "Mod1+Shift+n";
+      key-bindings.spawn-terminal = "Mod1+n";
       main.pad = "3x3 center";
     };
   };
