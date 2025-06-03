d52f75462abeeed34107a50f5fce05c9f56b246e
skoove <zie@skoove.dev>
2025-06-03 17:00:21 +1000

    ujnasd

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -21,6 +21,8 @@
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
+  nixpkgs.config.cudaSupport = true;
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
