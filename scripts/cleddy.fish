cf3c29beef265950f9a02fbda7bcb3dd5f76ba07
skoove <zie@skoove.dev>
2025-06-12 07:29:08 +1000

    edit scripts: stop write clipboard if empty input
    Added test in `cleddy` and `eddy` to stop them from overwriting the
    clipboard when the input was empty.

--- scripts/cleddy.fish
+++ scripts/cleddy.fish
@@ -1,8 +1,12 @@
 # cleddy: clipboard edit
 # opens clipboard in hx so you can edit it :D
-set temp_file (mktemp)
+set temp_file (mktemp).md
 
 wl-paste -n >$temp_file
 footclient $EDITOR $temp_file
+if not test -n "cat $temp_file"
+    rm $temp_file
+    return
+end
 wl-copy <$temp_file
 rm $temp_file
--- scripts/eddy.fish
+++ scripts/eddy.fish
@@ -1,6 +1,10 @@
 # eddy: open helix then save to clipbard
-set temp_file (mktemp)
+set temp_file (mktemp).md
 
 footclient $EDITOR $temp_file
+if not test -n "cat $temp_file"
+    rm $temp_file
+    return
+end
 wl-copy -n <$temp_file
 rm $temp_file
