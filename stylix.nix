62a30c6feaf048e8a42b4e539225ecc858a72ca9
skoove <zie@skoove.dev>
2025-06-12 07:30:16 +1000

    stylix: change wallpaper

--- stylix.nix
+++ stylix.nix
@@ -2,7 +2,7 @@
 {
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.image = ./wallpapers/solar-system.jpg;
+  stylix.image = ./wallpapers/cyberpunk.png;
   stylix.fonts = {
     monospace = {
       package = pkgs.nerd-fonts.jetbrains-mono;
