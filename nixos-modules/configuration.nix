051f0a4dbff7fc075ac301fb50606f53c83ddc05
skoove <zie@skoove.dev>
2025-06-19 10:02:25 +1000

    dont power bluetooth on boot

--- nixos-modules/configuration.nix
+++ nixos-modules/configuration.nix
@@ -101,7 +101,7 @@
   };
 
   hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth.powerOnBoot = false;
   services.blueman.enable = true;
 
   # Enable sound with pipewire.
