6972ad9e8f8725d084b207f007cba83e1617a4ac
skoove <zie@skoove.dev>
2025-06-22 13:52:50 +1000

    gaming: extra steam configurations

--- nixos-modules/gaming.nix
+++ nixos-modules/gaming.nix
@@ -1,6 +1,5 @@
 { pkgs , ... }:
 {
-  programs.gamescope.enable = true;
   programs.gamemode.enable = true;
   
   environment.systemPackages = with pkgs; [
@@ -11,5 +10,11 @@
     protonup-qt
   ];
 
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
 }
