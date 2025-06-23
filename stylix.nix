53272c0400b744ce309775d9bd3e14db2c03da6a
skoove <zie@skoove.dev>
2025-06-23 16:25:25 +1000

    stylix: change wallpaper
    change it to the photo of the surface of the moon

--- stylix.nix
+++ stylix.nix
@@ -2,7 +2,7 @@
 {
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.image = ./wallpapers/gruvbox-spire.png;
+  stylix.image = ./wallpapers/moon-surface.png;
   stylix.fonts = {
     monospace = {
       package = pkgs.nerd-fonts.jetbrains-mono;
