7e8731ed3de9b2b8dc602bda537d73af20f92f34
skoove <zie@skoove.dev>
2025-06-30 11:39:37 +1000

    remove cuda support option

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -21,8 +21,6 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  nixpkgs.config.cudaSupport = true;
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
