1df0d63e45234fdc670b444a30d00d638f82f6a0
skoove <zie@skoove.dev>
2025-07-17 21:15:41 +1000

    asd

--- home-modules/nushell.nix
+++ home-modules/nushell.nix
@@ -15,5 +15,26 @@
     settings = {
       buffer_editor = "${pkgs.helix}";
     };
+
+    shellAliases = {
+      gwip = "git commit -a -m 'wip'";
+      gco = "git checkout";
+      gsw = "git switch";
+      gcl = "git clone";
+      gpu = "git pull";
+      gg = "git log --oneline --graph";
+      gl = "git log";
+      gll = "git log --oneline";
+      gc = "git commit";
+      gp = "git push";
+      ga = "git add";
+      gs = "git status";
+      gd = "git diff";
+      gf = "git fetch";
+
+      lv = "loago view -m";
+      ld = "loago do";
+      lrm = "loago remove";
+    };
   };
 }
