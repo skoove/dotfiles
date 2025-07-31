f1dc3c95767a64f5c42fbc88eba029603f49962f
skoove <zie@skoove.dev>
2025-08-01 09:50:02 +1000

    skasd

--- home-modules/foot.nix
+++ home-modules/foot.nix
@@ -4,7 +4,14 @@
     enable = true;
     settings = {
       key-bindings.spawn-terminal = "Mod1+n";
+      mouse.hide-when-typing = true;
       main.pad = "3x3 center";
+
+      cursor = {
+        style = "underline";
+        blink = true;
+        blink-rate = 250;
+      };
     };
   };
 }
