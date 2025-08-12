c0faf4835a81ea7a73f7d23496c6b4f277895a38
skoove <zie@skoove.dev>
2025-08-12 13:01:40 +1000

    use emacs client instead of server

--- home-modules/emacs/default.nix
+++ home-modules/emacs/default.nix
@@ -2,12 +2,10 @@
 {
   stylix.targets.emacs.enable = false;
 
-  home.packages = [ pkgs.emacs ];
-
   # we use mkOutOfStoreSymLink here because i want hot reloading type shit
   home.file.".emacs".source = config.lib.file.mkOutOfStoreSymlink /home/zie/.dotfiles/home-modules/emacs/emacs.el;
 
-  services.emacs = {
+  programs.emacs = {
     enable = true;
     package = with pkgs; (
         (emacsPackagesFor emacs).emacsWithPackages (
