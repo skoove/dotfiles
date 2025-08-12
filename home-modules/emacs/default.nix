2b99df5827325fd0c55d1f49c31b903be99e39e0
skoove <zie@skoove.dev>
2025-08-12 14:49:09 +1000

    Reapply "emacs: turn on stylix themeing"
    This reverts commit 42381f93cebd5e17c3ac6deed7ac1f7e904d0da3.

--- home-modules/emacs/default.nix
+++ home-modules/emacs/default.nix
@@ -1,7 +1,5 @@
 { pkgs , config , ... }:
 {
-  stylix.targets.emacs.enable = false;
-
   # we use mkOutOfStoreSymLink here because i want hot reloading type shit
   home.file.".emacs".source = config.lib.file.mkOutOfStoreSymlink /home/zie/.dotfiles/home-modules/emacs/emacs.el;
 
