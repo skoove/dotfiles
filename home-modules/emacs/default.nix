e5e186d8cdeb20f51904ac98759e063817497857
skoove <zie@skoove.dev>
2025-08-12 12:51:00 +1000

    emacs type hsit

--- home-modules/emacs/default.nix
+++ home-modules/emacs/default.nix
@@ -4,6 +4,9 @@
 
   home.packages = [ pkgs.emacs ];
 
+  # we use mkOutOfStoreSymLink here because i want hot reloading type shit
+  home.file = ".emacs" = config.lib.mkOutOfStoreSymLink /home/zie/.dotfiles/home-modules/emacs/emacs.el;
+
   services.emacs = {
     enable = true;
     package = with pkgs; (
