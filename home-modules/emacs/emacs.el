e5719f81f8a3730247fbf49cb00d0af5f658965c
skoove <zie@skoove.dev>
2025-08-14 09:36:47 +1000

    emacs: change org-mode heading sizes

--- home-modules/emacs/emacs.el
+++ home-modules/emacs/emacs.el
@@ -73,10 +73,11 @@
     org-edit-src-content-indentation 0)
 
 (defun my-org-faces ()
-    (set-face-attribute 'org-todo nil :height 0.8)
-    (set-face-attribute 'org-level-1 nil :height 1.3)
-    (set-face-attribute 'org-level-2 nil :height 1.2)
-    (set-face-attribute 'org-level-3 nil :height 1.1))
+    (set-face-attribute 'org-level-1 nil :height 1.5)
+    (set-face-attribute 'org-level-2 nil :height 1.4)
+    (set-face-attribute 'org-level-3 nil :height 1.3)
+    (set-face-attribute 'org-level-4 nil :height 1.2)
+    (set-face-attribute 'org-level-5 nil :height 1.1))
 
 (add-hook 'org-mode-hook #'my-org-faces)
 
