d1d90e6011151c75f24430601a6ce90cd2130019
skoove <zie@skoove.dev>
2025-08-15 06:52:56 +1000

    emacs: change default things

--- home-modules/emacs/emacs.el
+++ home-modules/emacs/emacs.el
@@ -5,7 +5,8 @@
 
 (use-package general)
 (elcord-mode)
-(zone-when-idle 120)
+(require 'zone) (zone-when-idle 120)
+
 ;; vertico
 (use-package vertico
   :init
@@ -114,11 +115,10 @@
     org-edit-src-content-indentation 0)
 
 (defun my-org-faces ()
-    (set-face-attribute 'org-level-1 nil :height 1.5)
+    (set-face-attribute 'org-level-1 nil :height 1.6)
     (set-face-attribute 'org-level-2 nil :height 1.4)
-    (set-face-attribute 'org-level-3 nil :height 1.3)
-    (set-face-attribute 'org-level-4 nil :height 1.2)
-    (set-face-attribute 'org-level-5 nil :height 1.1))
+    (set-face-attribute 'org-level-3 nil :height 1.2)
+    (set-face-attribute 'org-level-4 nil :height 1.1))
 
 (add-hook 'org-mode-hook #'my-org-faces)
 
