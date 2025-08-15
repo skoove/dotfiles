cdde45edaf9d8cb4791c01fb0a7f599bfd9cd2ca
skoove <zie@skoove.dev>
2025-08-15 19:28:34 +1000

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/279ca5addcdcfa31ac852b3ecb39fc372684f426?narHash=sha256-UtYkukiGnPRJ5rpd4W/wFVrLMh8fqtNkqHTPgHEtrqU%3D' (2025-08-13)
      → 'github:nix-community/home-manager/11626a4383b458f8dc5ea3237eaa04e8ab1912f3?narHash=sha256-soZegto0xXzG2zYlu/zjknDHv0Z7tRS5EQs%2BZ/VRTBg%3D' (2025-08-15)
    • Updated input 'nixcord':
        'github:kaylorben/nixcord/0dcda512aa9d3fe2c915b53cb60106105947a598?narHash=sha256-QQl7c3BusCIHEmadfXLGiHccIJ0k9h1w6kYucGl4wOY%3D' (2025-08-14)
      → 'github:kaylorben/nixcord/dd183a04fd0990a9fd25bda7413180be122cf753?narHash=sha256-4XhUSD76ZPFZrmsN9qHX/XrWuuETi0qPxXtGk5EhnRA%3D' (2025-08-15)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/e9e0d35e5f735bf3d1e96815272f46fe7083232c?narHash=sha256-/bIVS2iP5mixEQWsaiiJ7EGLtk5Id9OehWbmTbzN6kE%3D' (2025-08-13)
      → 'github:NixOS/nixpkgs/a595dde4d0d31606e19dcec73db02279db59d201?narHash=sha256-V0j2S1r25QnbqBLzN2Rg/dKKil789bI3P3id7bDPVc4%3D' (2025-08-14)
    • Updated input 'spicetify-nix':
        'github:Gerg-L/spicetify-nix/fcbfc21572518c68317df992929b28df9a1d8468?narHash=sha256-oxWjZ/SfhCvHFNePZcUu%2BLcE5j4xxuIt/yaoaSvMZk0%3D' (2025-08-10)
      → 'github:Gerg-L/spicetify-nix/7d103eb173df0937a0a02f40692c018297cfb241?narHash=sha256-ssLRCiwcdkioOg9swgU2%2BPIhTR5SABnJxjTtaqbzPLo%3D' (2025-08-14)
    • Updated input 'stylix':
        'github:danth/stylix/c592717e9f713bbae5f718c784013d541346363d?narHash=sha256-hBSU7BEhd05y/pC9tliYjkFp8AblkbNEkPei229%2B0Pg%3D' (2025-08-12)
      → 'github:danth/stylix/928ca832d22ab3167b49dc5f4d52ff5d26b0b52a?narHash=sha256-kw6iLWUj6%2BfiEpuc8ntrIzJ2gdS36wIcRINbKU0AIbA%3D' (2025-08-14)

--- home-modules/obs.nix
+++ home-modules/obs.nix
@@ -2,13 +2,8 @@
 {
   programs.obs-studio = {
     enable = true;
-    programs.obs-studio.package = (pkgs.obs-studio.override {
+    package = (pkgs.obs-studio.override {
         cudaSupport = true;
     });
-
-    plugins = with pkgs.obs-studio-plugins; [
-      obs-pipewire-audio-capture
-      obs-vkcapture
-    ];
   };
 }
