5cd0710b655f1e3ae41d4c16aa8ae1d08c67b2a3
skoove <zie@skoove.dev>
2025-08-25 12:32:31 +1000

    change swaylock

--- scripts/lock-screen.fish
+++ scripts/lock-screen.fish
@@ -1,7 +1,7 @@
 set hour (date +%H)
 
 function lock
-    swaylock --clock --indicator $argv
+    swaylock $argv
 end
 
 swayidle timeout 300 "niri msg action power-off-monitors" \
@@ -9,12 +9,6 @@ swayidle timeout 300 "niri msg action power-off-monitors" \
 
 set sway_pid $last_pid
 
-if test $hour -ge 21
-    # if past 2100h lock to dark color
-    lock -c 282828
-else
-    # just blur a bit otherwise
-    lock --screenshot --effect-pixelate 10
-end
+lock --screenshot --effect-blur 5x5
 
 kill $sway_pid
