537a2ad8f4e95a1225ca59bf189c422329fd1ac6
skoove <zie@skoove.dev>
2025-09-04 10:59:45 +1000

    add gtk3

--- home-modules/home.nix
+++ home-modules/home.nix
@@ -111,7 +111,8 @@
         pkgs.inkscape-extensions.textext
       ];
     })
-
+    gtk3
+    
     # not tools at all but i dont want to nix run them
     nitch # pretty, mostly useless
     pulseaudio
