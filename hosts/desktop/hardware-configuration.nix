8929b500fd1bd9da7a79c23559df6a8e3069bc96
skoove <zie@skoove.dev>
2025-09-05 13:00:14 +1000

    desktop: downgrade nvidia drivers
    fuck nvidia
    
    thank you @iynaix

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -23,7 +23,7 @@
     powerManagement.enable = true; 
     open = false;                  
     nvidiaSettings = false;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   boot.initrd.availableKernelModules = [
