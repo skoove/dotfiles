962d49cbe41d535bf1aa916b2f555e9ae4b58c6b
skoove <zie@skoove.dev>
2025-10-30 14:27:47 +1000

    fix build

--- nixos-modules/udev.nix
+++ nixos-modules/udev.nix
@@ -3,7 +3,7 @@
 
   services.udev = {
     enable = true;
-    packages = [ pkgs.android-udev-rules ];
+    # packages = [ pkgs.android-udev-rules ];
     extraRules = ''SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", ATTR{idProduct}=="2e81", MODE="0666", GROUP="plugdev"'';
   };
 }
