49663ae3f84f3cda50e780d94e0f62a7748bedb4
skoove <zie@skoove.dev>
2025-11-15 13:40:22 +1000

    Revert "annotation mono"
    This reverts commit dc1ca5b5893795b02698fc8b4257742e3bf27c1e.

--- nixos-modules/stylix.nix
+++ nixos-modules/stylix.nix
@@ -5,8 +5,8 @@
   # stylix.image = "${inputs.wallpapers}/forest.jpg";
   stylix.fonts = {
     monospace = {
-      package = pkgs.annotation-mono;
-      name = "annotation mono";
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrainsMono NF";
     };
 
     sansSerif = {
