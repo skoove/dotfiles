da6b7b616eb53cb60699f199da8ada5486204d8e
skoove <zie@skoove.dev>
2025-11-16 17:18:20 +1000

    helix: typst lsp no compile on save

--- home-modules/helix.nix
+++ home-modules/helix.nix
@@ -47,8 +47,6 @@
 
       language-server.tinymist.config = {
         formatterMode = "typstyle";
-        exportPdf = "onSave";
-        outputPath = "$root/target/$dir/$name";
         preview.browsing.args = ["--data-plane-host=127.0.0.1:0" "--invert-colors=never" "--open"];
       };
       
