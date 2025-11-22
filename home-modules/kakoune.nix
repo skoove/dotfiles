06b663f0ca767ab364e45ce50872bccc997a467e
skoove <zie@skoove.dev>
2025-11-22 11:35:08 +1000

    kak: change terminal assistant to cat

--- home-modules/kakoune.nix
+++ home-modules/kakoune.nix
@@ -1,8 +1,10 @@
 { pkgs , ... }:
 {
-  home.packages = [ pkgs.kakoune ];
+  home.packages = [ (pkgs.kakoune.override { plugins = [];  }) ];
 
   home.file.".config/kak/kakrc".source = pkgs.writeText "kakrc" ''
     colorscheme gruvbox-dark
+
+    set-option global ui_options terminal_assistant=cat
   '';
 }
