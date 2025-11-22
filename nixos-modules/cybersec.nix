dd9dcb6892643a4d4b32743caea6790d5e1bfe4a
skoove <zie@skoove.dev>
2025-11-22 22:29:20 +1000

    more basic kak config

--- nixos-modules/cybersec.nix
+++ nixos-modules/cybersec.nix
@@ -1,9 +1,5 @@
 { pkgs , ... }:
 {
-  # hi people looking at my config, i am not a script kiddie, i am actually
-  # learning (responsibly! i have a homelab [1])
-  #
-  # [1] https://github.com/skoove/server-configurations
   environment.systemPackages = with pkgs; [
     nmap
     whois
